599b8a3be69997f11f9a21b72e210b7ff5f07a9c
AdityaJ42 <adit98@gmail.com>
2022-12-04 14:25:43 -0800

    fixed weird spacing

--- battleship/src/UI.hs
+++ battleship/src/UI.hs
@@ -1,68 +1,72 @@
 {-# LANGUAGE OverloadedStrings #-}
-module UI (attrs, draw) where 
 
-import Brick.Types ( Widget, Padding (..) )
-import Brick.Widgets.Core ( str, withAttr, vBox, joinBorders, hBox, withBorderStyle, padAll, padRight, padLeftRight, txt, hLimit, textWidth, txtWrap, setAvailableSize, fill )
-import Brick.AttrMap ( AttrMap, AttrName, attrMap )
-import Brick.Util ( on, fg )
+module UI (attrs, draw) where
+
+import Brick.AttrMap (AttrMap, AttrName, attrMap)
+import Brick.Types (Padding (..), Widget)
+import Brick.Util (fg, on)
 import Brick.Widgets.Border (border, borderWithLabel, hBorder)
 import Brick.Widgets.Border.Style (unicode)
-import qualified Graphics.Vty as V
-
-import State (State(..))
-import Types (ResourceName)
-import Game (Coordinate, isHit, Game, isSetup, Status(..), getOceanStatus, getTargetStatus)
+import Brick.Widgets.Core (fill, hBox, hLimit, joinBorders, padAll, padLeftRight, padRight, setAvailableSize, str, textWidth, txt, txtWrap, vBox, withAttr, withBorderStyle)
+import Brick.Widgets.Table (ColumnAlignment (AlignCenter), RowAlignment (..), alignCenter, alignLeft, renderTable, setDefaultColAlignment, setDefaultRowAlignment, table)
 import Data.Bits (xor)
-import Brick.Widgets.Table (table, renderTable, alignCenter, alignLeft, setDefaultColAlignment, ColumnAlignment (AlignCenter), RowAlignment (..), setDefaultRowAlignment)
 import Data.Char (chr, ord)
 import qualified Data.Text as T
-
+import Game (Coordinate, Game, Status (..), getOceanStatus, getTargetStatus, isHit, isSetup)
+import qualified Graphics.Vty as V
+import State (State (..))
+import Types (ResourceName)
 
 -- Returns the attribute style map for the application
 attrs :: AttrMap
-attrs = attrMap V.defAttr 
-  [ (errorAttr, fg V.red)
-  , (boatAttr, V.blue `on` V.blue)
-  , (hitAttr, V.red `on` V.red)
-  , (missAttr, V.white `on` V.white)
-  ]
+attrs =
+  attrMap
+    V.defAttr
+    [ (errorAttr, fg V.red),
+      (boatAttr, V.blue `on` V.blue),
+      (hitAttr, V.red `on` V.red),
+      (missAttr, V.white `on` V.white)
+    ]
 
 -- Draws the TUI from the given state
 draw :: State -> [Widget ResourceName]
 draw st = [draw' st]
 
-draw' :: State -> Widget ResourceName 
-draw' st = vBox $ 
-  let grids = drawGrids st
-  in [grids, txtBoxes]
+draw' :: State -> Widget ResourceName
+draw' st =
+  vBox $
+    let grids = drawGrids st
+     in [grids, txtBoxes]
 
 -- Draws the grids
 drawGrids :: State -> Widget ResourceName
-drawGrids st = let g = [drawGrid st oceanGrid]
-                   g' = [drawGrid st targetGrid]
-               in hBox $ g ++ g'
+drawGrids st =
+  let g = [drawGrid st oceanGrid]
+      g' = [drawGrid st targetGrid]
+   in hBox $ g ++ g'
 
 -- Draws the grid
 drawGrid :: State -> String -> Widget ResourceName
-drawGrid st h = 
+drawGrid st h =
   withBorderStyle unicode $
-  borderWithLabel (padLeftRight 1 $ str h) $ 
-  let grid = constructGrid
-      oGrid = gridMap (transform st) grid -- Add attributes here
-      tGrid = gridMap (transform' st) grid -- Add attributes here
-      wGrid = if h == oceanGrid then oGrid else tGrid
-      widget = padRight (Pad 1) $ 
-        renderTable $ 
-          setDefaultRowAlignment AlignMiddle $
-          setDefaultColAlignment AlignCenter $
-          table wGrid
-  in widget
-  
+    borderWithLabel (padLeftRight 1 $ str h) $
+      let grid = constructGrid
+          oGrid = gridMap (transform st) grid -- Add attributes here
+          tGrid = gridMap (transform' st) grid -- Add attributes here
+          wGrid = if h == oceanGrid then oGrid else tGrid
+          widget =
+            padRight (Pad 1) $
+              renderTable $
+                setDefaultRowAlignment AlignMiddle $
+                  setDefaultColAlignment AlignCenter $
+                    table wGrid
+       in widget
+
 -- Construct grid
 constructGrid :: [[Coordinate]]
-constructGrid = map f [-1..9]
+constructGrid = map f [-1 .. 9]
   where
-    f i = [(i, j) | j <- [-1..9]]
+    f i = [(i, j) | j <- [-1 .. 9]]
 
 -- Map all values in a 2d-grid
 gridMap :: (a -> b) -> [[a]] -> [[b]]
@@ -70,22 +74,27 @@ gridMap f xs = map (\x -> map f x) xs
 
 -- Transforms Coordinates to widgets (Ocean)
 transform :: State -> Coordinate -> Widget ResourceName
-transform st (r, c) = if (r == -1) `xor` (c == -1) then 
-                        if r == -1 then str $ show c
-                        else str [chr $ ord 'a' + r]
-                      else
-                        padLeftRight 1 $ 
-                        withAttr (mapStatus $ getOceanStatus (r, c) $ game st) drawEmpty
-
+transform st (r, c) =
+  if (r == -1) `xor` (c == -1)
+    then
+      if r == -1
+        then str $ show c
+        else str [chr $ ord 'a' + r]
+    else
+      padLeftRight 1 $
+        withAttr (mapStatus $ getOceanStatus (r, c) $ game st) drawEmpty
 
 -- Transforms Coordinates to widgets (Target)
 transform' :: State -> Coordinate -> Widget ResourceName
-transform' st (r, c) = if (r == -1) `xor` (c == -1) then 
-                        if r == -1 then str $ show c
-                        else str [chr $ ord 'a' + r]
-                      else
-                        padLeftRight 1 $ 
-                        withAttr (mapStatus $ getTargetStatus (r, c) $ game st) drawEmpty
+transform' st (r, c) =
+  if (r == -1) `xor` (c == -1)
+    then
+      if r == -1
+        then str $ show c
+        else str [chr $ ord 'a' + r]
+    else
+      padLeftRight 1 $
+        withAttr (mapStatus $ getTargetStatus (r, c) $ game st) drawEmpty
 
 -- Draws an empty cell
 drawEmpty :: Widget ResourceName
@@ -94,9 +103,9 @@ drawEmpty = str "  "
 -- Attributes
 
 boatAttr, hitAttr, missAttr, emptyAttr :: AttrName
-boatAttr  = "boatAttr"
-hitAttr   = "hitAttr"
-missAttr  = "missAttr"
+boatAttr = "boatAttr"
+hitAttr = "hitAttr"
+missAttr = "missAttr"
 emptyAttr = "emptyAttr"
 
 errorAttr :: AttrName
@@ -104,14 +113,14 @@ errorAttr = "error"
 
 -- Headers
 oceanGrid, targetGrid, input, info :: String
-oceanGrid  = "Ocean Grid"
+oceanGrid = "Ocean Grid"
 targetGrid = "Target Grid"
-input      = "Input"
-info       = "Info"
+input = "Input"
+info = "Info"
 
 -- Mappers
 mapStatus :: Status -> AttrName
 mapStatus H = hitAttr
-mapStatus M = missAttr 
+mapStatus M = missAttr
 mapStatus B = boatAttr
 mapStatus E = emptyAttr
